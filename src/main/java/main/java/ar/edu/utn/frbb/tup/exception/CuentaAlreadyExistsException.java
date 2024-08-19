ba9a2055a0da761388a2103f904b67926d5fd7c6
joaquin <joaquinhernandezl.2@gmail.com>
2024-08-19 20:40:55 -0300

    commits 1/8 - 19/8

--- src/main/java/main/java/ar/edu/utn/frbb/tup/exception/CuentaAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package main.java.ar.edu.utn.frbb.tup.exception;
-
-public class CuentaAlreadyExistsException extends Throwable {
-    public CuentaAlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- src/main/java/main/java/ar/edu/utn/frbb/tup/exception/TipoCuentaAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package main.java.ar.edu.utn.frbb.tup.exception;
-
-public class TipoCuentaAlreadyExistsException extends Throwable {
-    public TipoCuentaAlreadyExistsException(String message) {
-        super(message);
-    }
-}
